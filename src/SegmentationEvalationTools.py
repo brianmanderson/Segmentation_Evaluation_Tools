2fee4a9a1eeaf74dd7ed17ca1d3af35e9f8496ed
markba122@gmail.com <bmanderson@mdanderson.org>
2020-09-30 20:26:57 -0500

    add overlap metrics and false positive false volume

--- src/SegmentationEvalationTools.py
+++ src/SegmentationEvalationTools.py
@@ -3,7 +3,7 @@ __author__ = 'Brian M Anderson'
 import SimpleITK as sitk
 import numpy as np
 from enum import Enum
-
+from Deep_Learning.Base_Deeplearning_Code.Plot_And_Scroll_Images.Plot_Scroll_Images import plot_scroll_Image
 
 class OverlapMeasures(Enum):
     jaccard, dice, volume_similarity, false_negative, false_positive = range(5)
@@ -13,10 +13,10 @@ class SurfaceDistanceMeasures(Enum):
     mean_surface_distance, median_surface_distance, std_surface_distance, max_surface_distance = range(4)
 
 
-def identify_overlap_metrics(prediction_handle, ground_truth_handle, perform_distance_measures=False):
+def identify_overlap_metrics(prediction_handle, truth_handle, perform_distance_measures=False):
     out_dict = {}
     overlap_measures_filter = sitk.LabelOverlapMeasuresImageFilter()
-    overlap_measures_filter.Execute(ground_truth_handle, prediction_handle)
+    overlap_measures_filter.Execute(truth_handle, prediction_handle)
     out_dict[OverlapMeasures.jaccard.name] = overlap_measures_filter.GetJaccardCoefficient()
     out_dict[OverlapMeasures.dice.name] = overlap_measures_filter.GetDiceCoefficient()
     out_dict[OverlapMeasures.volume_similarity.name] = overlap_measures_filter.GetVolumeSimilarity()
@@ -24,8 +24,8 @@ def identify_overlap_metrics(prediction_handle, ground_truth_handle, perform_dis
     out_dict[OverlapMeasures.false_positive.name] = overlap_measures_filter.GetFalsePositiveError()
     if perform_distance_measures:
         statistics_image_filter = sitk.StatisticsImageFilter()
-        reference_surface = sitk.LabelContour(ground_truth_handle)
-        reference_distance_map = sitk.Abs(sitk.SignedMaurerDistanceMap(ground_truth_handle, squaredDistance=False,
+        reference_surface = sitk.LabelContour(truth_handle)
+        reference_distance_map = sitk.Abs(sitk.SignedMaurerDistanceMap(truth_handle, squaredDistance=False,
                                                                        useImageSpacing=True))
 
         statistics_image_filter.Execute(reference_surface)
@@ -68,6 +68,66 @@ def identify_overlap_metrics(prediction_handle, ground_truth_handle, perform_dis
             all_surface_distances)
         out_dict[SurfaceDistanceMeasures.max_surface_distance.name] = np.max(
             all_surface_distances)
+    return out_dict
+
+
+def determine_false_positive_rate_and_false_volume(prediction_handle, truth_handle):
+    '''
+    :param prediction_handle:
+    :param truth_handle:
+    :return: a dictionary with False Positive Volume (cc)
+    False Predictions Volume (cc), this is the volume not connected to any truth prediction
+    Over Segmentation Volume (cc), this is the volume over-segmented on ground truth
+    '''
+    prediction = sitk.GetArrayFromImage(prediction_handle)
+    truth = sitk.GetArrayFromImage(truth_handle)
+
+    out_dict = {}
+    spacing = np.prod(truth_handle.GetSpacing())
+    volume = np.sum(sitk.GetArrayFromImage(truth_handle)) * spacing / 1000
+    out_dict['Volume (cc)'] = volume
+    '''
+    False Positive Volume (cc) is the easiest one, just subtract them
+    '''
+    total_difference = np.sum(prediction - truth > 0) * spacing / 1000
+    false_volume = total_difference
+    out_dict['False Volume (cc)'] = false_volume
+    '''
+    Next, we want to grow the prediction volume that touches ground truth, so multiple the prediction and ground truth
+    '''
+    overlap = prediction * truth
+    '''
+    See if there is any overlap at all first
+    '''
+    stats = sitk.LabelShapeStatisticsImageFilter()
+    Connected_Component_Filter = sitk.ConnectedComponentImageFilter()
+    if np.max(overlap) == 0:
+        labeled_difference = Connected_Component_Filter.Execute(prediction_handle)
+        stats.Execute(labeled_difference)
+        difference_labels = stats.GetLabels()
+        difference = prediction
+    else:
+        seeds = np.transpose(np.asarray(np.where(overlap > 0)))[..., ::-1]
+        seeds = [[int(i) for i in j] for j in seeds]
+        stats = sitk.LabelShapeStatisticsImageFilter()
+        Connected_Component_Filter = sitk.ConnectedComponentImageFilter()
+        '''
+        The seeds represent the starting points of the ground truth, we'll use these to determine truth from false
+        '''
+        Connected_Threshold = sitk.ConnectedThresholdImageFilter()
+        Connected_Threshold.SetUpper(2)
+        Connected_Threshold.SetLower(1)
+        Connected_Threshold.SetSeedList(seeds)
+        seed_grown_pred = sitk.GetArrayFromImage(Connected_Threshold.Execute(prediction_handle))
+        difference = prediction - seed_grown_pred  # Now we have our prediction, subtracting those that include truth
+        labeled_difference = Connected_Component_Filter.Execute(sitk.GetImageFromArray(difference.astype('int')))
+        stats.Execute(labeled_difference)
+        difference_labels = stats.GetLabels()
+    out_dict['Number False Positives'] = len(difference_labels)
+    difference -= truth  # Subtract truth in case seed didn't land
+    false_prediction_volume = np.sum(difference > 0) * spacing / 1000
+    out_dict['False Prediction Volume (cc)'] = false_prediction_volume
+    out_dict['Over Segmentation Volume (cc)'] = false_volume - false_prediction_volume
     return out_dict
 
 
